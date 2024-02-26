527004ef09e44516b7550f1d51f95b498b7f40a0
2dilution2 <2dilution2@gmail.com>
2024-02-26 18:16:05 +0900

    [build] server initial commit

---
+++ GIS_server/src/main/java/org/pyriboo/gis_server/GisServerApplication.java
@@ -0,0 +1,13 @@
+package org.pyriboo.gis_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GisServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GisServerApplication.class, args);
+	}
+
+}
---
+++ GIS_server/src/test/java/org/pyriboo/gis_server/GisServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pyriboo.gis_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GisServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
