0fe1d9655505374fb6abc0ed14a18bffbafa3a0a
2dilution2 <2dilution2@gmail.com>
2024-02-27 13:34:40 +0900

    [feat] password encode setting

--- GIS_server/src/main/java/org/pyriboo/gis_server/domain/users/service/UserServiceImpl.java
+++ GIS_server/src/main/java/org/pyriboo/gis_server/domain/users/service/UserServiceImpl.java
@@ -3,6 +3,7 @@ package org.pyriboo.gis_server.domain.users.service;
 import org.pyriboo.gis_server.domain.users.model.Users;
 import org.pyriboo.gis_server.domain.users.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,14 +13,17 @@ import java.util.Optional;
 public class UserServiceImpl implements UserService {
 
 	private final UserRepository userRepository;
+	private final PasswordEncoder passwordEncoder;
 
 	@Autowired
-	public UserServiceImpl(UserRepository userRepository) {
+	public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
 		this.userRepository = userRepository;
+		this.passwordEncoder = passwordEncoder;
 	}
 
 	@Override
 	public Users saveUser(Users user) {
+		user.setPassword(passwordEncoder.encode(user.getPassword()));
 		return userRepository.save(user);
 	}
 
