718054e12e8e2da9f81d50b75c9302db99813db3
2dilution2 <2dilution2@gmail.com>
2024-02-27 15:52:57 +0900

    [fix] User service update

--- GIS_server/src/main/java/org/pyriboo/gis_server/domain/users/service/UserService.java
+++ GIS_server/src/main/java/org/pyriboo/gis_server/domain/users/service/UserService.java
@@ -1,13 +1,9 @@
 package org.pyriboo.gis_server.domain.users.service;
 
 import java.util.List;
-
-import org.pyriboo.gis_server.domain.users.dto.UserReq;
-import org.pyriboo.gis_server.domain.users.dto.UserRes;
+import java.util.Optional;
 
 import org.pyriboo.gis_server.domain.users.model.Users;
-import java.util.List;
-import java.util.Optional;
 
 public interface UserService {
 
