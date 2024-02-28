3f00a53190d64b81ea5d6e7b2b01abd0d2bebb2e
2dilution2 <2dilution2@gmail.com>
2024-02-28 22:41:00 +0900

    [feat] LyricsAndChords setting

--- GIS_server/src/main/java/org/pyriboo/gis_server/domain/song/model/LyricsAndChords.java
+++ GIS_server/src/main/java/org/pyriboo/gis_server/domain/song/model/LyricsAndChords.java
@@ -1,5 +1,15 @@
 package org.pyriboo.gis_server.domain.song.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+// subDocument
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class LyricsAndChords {
 
 	private String lyrics;
