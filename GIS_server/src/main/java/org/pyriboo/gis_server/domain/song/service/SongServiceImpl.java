68e797d741ab070233f7fa23520d880e66866567
2dilution2 <2dilution2@gmail.com>
2024-02-28 22:36:27 +0900

    [feat] song service update

--- GIS_server/src/main/java/org/pyriboo/gis_server/domain/song/service/SongServiceImpl.java
+++ GIS_server/src/main/java/org/pyriboo/gis_server/domain/song/service/SongServiceImpl.java
@@ -1,10 +1,12 @@
 package org.pyriboo.gis_server.domain.song.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.pyriboo.gis_server.domain.song.dto.SongReq;
 import org.pyriboo.gis_server.domain.song.dto.SongRes;
+import org.pyriboo.gis_server.domain.song.model.Song;
 import org.pyriboo.gis_server.domain.song.repository.SongRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,30 +19,55 @@ public class SongServiceImpl implements SongService {
 
 	@Override
 	public SongRes createSong(SongReq songReq) {
+		Song song = new Song();
+		song.setTitle(songReq.getTitle());
+		song.setArtist(songReq.getArtist());
+		song.setHashtags(songReq.getHashtags());
+		Song savedSong = songRepository.save(song);
 
-		return new SongRes();
+		return convertToDto(savedSong);
 	}
 
 	@Override
 	public List<SongRes> getAllSongs() {
-
-		return songRepository.findAll().stream().map(song -> new SongRes()).collect(Collectors.toList());
+		return songRepository.findAll().stream()
+			.map(this::convertToDto)
+			.collect(Collectors.toList());
 	}
 
 	@Override
 	public SongRes getSongById(String id) {
-
-		return new SongRes();
+		Optional<Song> song = songRepository.findById(id);
+		return song.map(this::convertToDto).orElse(null);
 	}
 
 	@Override
 	public SongRes updateSong(String id, SongReq songReq) {
+		Optional<Song> existingSong = songRepository.findById(id);
+		if (existingSong.isPresent()) {
+			Song song = existingSong.get();
+			song.setTitle(songReq.getTitle());
+			song.setArtist(songReq.getArtist());
+			song.setHashtags(songReq.getHashtags());
 
-		return new SongRes();
+			Song updatedSong = songRepository.save(song);
+			return convertToDto(updatedSong);
+		}
+		return null;
 	}
 
 	@Override
 	public void deleteSong(String id) {
+		songRepository.deleteById(id);
+	}
 
+	private SongRes convertToDto(Song song) {
+		SongRes songRes = new SongRes();
+		songRes.setId(song.getId());
+		songRes.setTitle(song.getTitle());
+		songRes.setArtist(song.getArtist());
+		songRes.setHashtags(song.getHashtags());
+
+		return songRes;
 	}
 }
