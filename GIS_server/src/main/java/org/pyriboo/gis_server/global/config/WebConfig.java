f37ae6b6c40c53b9573c401a36f3188428fcbac1
2dilution2 <2dilution2@gmail.com>
2024-03-04 20:42:20 +0900

    [feat] spring security setting

---
+++ GIS_server/src/main/java/org/pyriboo/gis_server/global/config/WebConfig.java
@@ -0,0 +1,19 @@
+package org.pyriboo.gis_server.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("http://localhost:3000")
+			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+			.allowedHeaders("*") // 모든 헤더 허용
+			.allowCredentials(true) // 쿠키를 포함한 요청 허용
+			.maxAge(3600); // pre-flight 요청 캐시 시간 (초 단위)
+	}
+}
